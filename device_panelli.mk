9fc00cc8eed5dcd85aef664a410f12bd5e9f8673
Ayush <ayushbisht5663@gmail.com>
2020-06-23 11:49:09 +0530

    panelli: rename

--- device_panelli.mk
+++ device_panelli.mk
@@ -24,15 +24,15 @@ PRODUCT_COPY_FILES += \
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/init.project.rc:root/init.project.rc \
-    $(LOCAL_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
-    $(LOCAL_PATH)/rootdir/ueventd.mt6735.rc:root/ueventd.mt6735.rc \
-    $(LOCAL_PATH)/rootdir/init.mt6735.usb.rc:root/init.mt6735.usb.rc \
-    $(LOCAL_PATH)/rootdir/init.mt6735.rc:root/init.mt6735.rc \
-    $(LOCAL_PATH)/rootdir/fstab.mt6735:root/fstab.mt6735 \
-    $(LOCAL_PATH)/rootdir/init.microtrust.rc:root/init.microtrust.rc \
-    $(LOCAL_PATH)/rootdir/init.connectivity.rc:root/init.connectivity.rc \
-    $(LOCAL_PATH)/rootdir/init.mt6735.power.rc:root/init.mt6735.power.rc
+    $(LOCAL_PATH)/ramdisk/init.project.rc:root/init.project.rc \
+    $(LOCAL_PATH)/ramdisk/init.modem.rc:root/init.modem.rc \
+    $(LOCAL_PATH)/ramdisk/ueventd.mt6735.rc:root/ueventd.mt6735.rc \
+    $(LOCAL_PATH)/ramdisk/init.mt6735.usb.rc:root/init.mt6735.usb.rc \
+    $(LOCAL_PATH)/ramdisk/init.mt6735.rc:root/init.mt6735.rc \
+    $(LOCAL_PATH)/ramdisk/fstab.mt6735:root/fstab.mt6735 \
+    $(LOCAL_PATH)/ramdisk/init.microtrust.rc:root/init.microtrust.rc \
+    $(LOCAL_PATH)/ramdisk/init.connectivity.rc:root/init.connectivity.rc \
+    $(LOCAL_PATH)/ramdisk/init.mt6735.power.rc:root/init.mt6735.power.rc
 
 # Hardware Permissions
 PRODUCT_COPY_FILES += \
