705ad65d190d84660a4874e9e15891f37a6ed100
Ayush <ayushbisht5663@gmail.com>
2020-06-30 13:18:52 +0530

    panelli: update configs for Mokee

--- mokee.mk
+++ mokee.mk
@@ -1,11 +1,8 @@
-# Specify phone tech before including full_phone	
-$(call inherit-product, vendor/lineage/config/telephony.mk)
-
 # Release name
 PRODUCT_RELEASE_NAME := panelli
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common mokee stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/moto/panelli/device_panelli.mk)
@@ -17,7 +14,7 @@ TARGET_SCREEN_HEIGHT := 1280
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := panelli
-PRODUCT_NAME := lineage_panelli
+PRODUCT_NAME := mk_panelli
 PRODUCT_BRAND := moto
 PRODUCT_MODEL := panelli
 PRODUCT_MANUFACTURER := moto
